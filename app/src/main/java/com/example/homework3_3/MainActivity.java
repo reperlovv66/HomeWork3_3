64343f92242123c3f45b6f4c01d9b2e73d31cb1f
Egor Iakimov <egoriakimov@Mac-mini-Egor.local>
2023-03-29 18:43:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/homework3_3/MainActivity.java
@@ -0,0 +1,158 @@
+package com.example.homework3_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@SuppressLint("UseSwitchCompatOrMaterialCode")
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
+
+    private final String DATA_STREAM = "http://ep128.hostingradio.ru:8030/ep128";
+    private final String DATA_SD = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC) + "/music.mp3";
+    private String nameAudio = "";
+
+    private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
+    private Toast toast;
+
+    private TextView textOut;
+    private Switch switchLoop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textOut = findViewById(R.id.textOut);
+        switchLoop = findViewById(R.id.switchLoop);
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        switchLoop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(mediaPlayer != null)
+                    mediaPlayer.setLooping(isChecked);
+            }
+        });
+    }
+
+    public void onClickSource(View view) {
+
+        releaseMediaPlayer();
+
+        try {
+            switch (view.getId()) {
+                case R.id.btnStream:
+                    toast = Toast.makeText(this, "Запущен поток аудио", Toast.LENGTH_SHORT);
+                    toast.show();
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_STREAM);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.setOnPreparedListener(this);
+                    mediaPlayer.prepareAsync();
+                    nameAudio = "РАДИО";
+                    break;
+                case R.id.btnRAW:
+                    toast = Toast.makeText(this, "Запущен аудио-файл с памяти телефона", Toast.LENGTH_SHORT);
+                    toast.show();
+                    mediaPlayer = MediaPlayer.create(this, R.raw.gods_plan);
+                    mediaPlayer.start();
+                    nameAudio = "Drake - Gods plan";
+                    break;
+                case R.id.btnSD:
+                    toast = Toast.makeText(this, "Запущен аудио-файл с SD-карты", Toast.LENGTH_SHORT);
+                    toast.show();
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_SD);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            toast = Toast.makeText(this, "Источник информации не найден", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        if (mediaPlayer == null) return;
+
+        mediaPlayer.setLooping(switchLoop.isChecked());
+        mediaPlayer.setOnCompletionListener(this);
+    }
+
+    public void onClick(View view) {
+        if (mediaPlayer == null) return;
+
+        switch (view.getId()) {
+            case R.id.btnResume:
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                }
+                break;
+            case R.id.btnPause:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                }
+                break;
+            case R.id.btnStop:
+                mediaPlayer.stop();
+                break;
+            case R.id.btnForward:
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 5000);
+                break;
+            case R.id.btnBack:
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 5000);
+                break;
+        }
+        textOut.setText(nameAudio + "\n(проигрывание " + mediaPlayer.isPlaying() + ", время " + mediaPlayer.getCurrentPosition()
+                + ",\nповтор " + mediaPlayer.isLooping() + ", громкость " + audioManager.getStreamVolume(AudioManager.STREAM_MUSIC) + ")");
+    }
+
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mediaPlayer.start();
+        toast = Toast.makeText(this, "Старт медиа-плейера", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        toast = Toast.makeText(this, "Отключение медиа-плейера", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseMediaPlayer();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.release();
+                mediaPlayer = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
